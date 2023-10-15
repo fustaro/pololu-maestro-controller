79950936ce6f7682c245356eb3b73aab526f8d08
fustaro <the3dprintspace@gmail.com>
2023-10-15 11:24:13 +0100

    bump servo-core version

--- src/MaestroServoController.ts
+++ src/MaestroServoController.ts
@@ -1,37 +1,40 @@
 import PololuMaestro from "pololu-maestro";
-import { ServoControllerFactory, PwmWriter, HardwareInterface, IServoController } from "@fustaro/servo-core";
+import { ServoControllerFactory, ServoDriver, HardwareInterface, IServoController } from "@fustaro/servo-core";
 
-interface MaestroServoDriver extends PwmWriter {
+interface MaestroServoDriver extends ServoDriver {
     ready: boolean;
 }
  
 export const getOrCreateMaestroController = (uniqueHardwareName: string, portName: string, channels: number, baudRate: number | false = false, debug: boolean = false): IServoController => {
-    let controller = ServoControllerFactory.get(uniqueHardwareName);
+    const controller = ServoControllerFactory.get(uniqueHardwareName);
 
-    if(!controller){
-        const pololuMaestro = new PololuMaestro(portName, baudRate, debug);
-
-        const servoDriver: MaestroServoDriver = {
-            ready: false,
-            writePwm: (channel: number, pwm: number, callback?: Function): void => {
-                if(servoDriver.ready) pololuMaestro.setTarget(channel, pwm, callback);
-            }
-        }
-
-        pololuMaestro.on("disconnected", () => {
-            console.log(`MaestroServoController: ${uniqueHardwareName} has disconnected`);
-            servoDriver.ready = false;
-        });
-        
-        pololuMaestro.on("ready", () => {
-            console.log(`MaestroServoController: ${uniqueHardwareName} is ready`);
-            servoDriver.ready = true;
-        });
-
-        const maestroServoHardwareDriver = new HardwareInterface(servoDriver, uniqueHardwareName, channels, true);
-
-        controller = ServoControllerFactory.create(maestroServoHardwareDriver);
+    if(controller){
+        return controller;
     }
 
-    return controller;
+    const pololuMaestro = new PololuMaestro(portName, baudRate, debug);
+
+    const servoDriver: MaestroServoDriver = {
+        ready: false,
+        writePwm: (channel: number, pwm: number, callback?: Function): void => {
+            if(servoDriver.ready) pololuMaestro.setTarget(channel, pwm, callback);
+        },
+        dispose: () => {
+            pololuMaestro.close();
+        }
+    }
+
+    pololuMaestro.on("disconnected", () => {
+        console.log(`MaestroServoController: ${uniqueHardwareName} has disconnected`);
+        servoDriver.ready = false;
+    });
+    
+    pololuMaestro.on("ready", () => {
+        console.log(`MaestroServoController: ${uniqueHardwareName} is ready`);
+        servoDriver.ready = true;
+    });
+
+    const maestroServoHardwareDriver = new HardwareInterface(servoDriver, uniqueHardwareName, channels, true);
+
+    return ServoControllerFactory.create(maestroServoHardwareDriver);
 }
